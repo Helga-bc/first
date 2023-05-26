5998f435b993fbe4384635e4ed39d1dc3c147e7d
Helga <helga-bc@yandex.by>
2023-05-26 21:53:39 +0300

    добавление новых полей

--- books/models.py
+++ books/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    year = models.IntegerField()
+    year = models.IntegerField(blank=True)
+    raiting = models.IntegerField(default=0)
+
+    publisher = models.CharField(max_length=50, null=True)
+
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True, related_name='books')
 
     def __str__(self):
         return f"Книга:{self.id} Название: {self.title} Автор: {self.author} Год: {self.year} "
@@ -12,3 +17,5 @@ class Book(models.Model):
 class Genre(models.Model):
     title = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"Жанр {self.id}, {self.title}"
